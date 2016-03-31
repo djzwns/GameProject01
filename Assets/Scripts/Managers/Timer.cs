16600ceb2d0a4e604507dac402475450b9e7a40f
djzwns <hjjshg@game.hoseo.edu>
2016-03-31 23:01:16 +0900

    타이머 완성

--- Assets/Scripts/Managers/Timer.cs
+++ Assets/Scripts/Managers/Timer.cs
@@ -8,17 +8,25 @@ public class Timer : MonoBehaviour {
     public float closeTime;
     // ------------------------------
 
-    public GameObject endPoint;  // 개방 될 문임
+    public GameObject endPoint;     // 개방 될 문임
+
+    // 그래픽으로 시간의 흐름을 보여줌 -----------------------------------
+    public Scrollbar openTimer;     // 오픈 타이머
+    public Scrollbar currentTimer;  // 현재 시간 타이머
 
     public Text openTimeText;    // 개방시간을 출력해줄 gui text
     public Text currentTimeText; // 시간을 출력해줄 gui text
+    public Text timeCycleText;   // 몇 번 초과 됐는지 출력
+    // -------------------------------------------------------------------
 
     float currentTime;           // 현재 시간
+    int timeCycle;               // 시간 초과된 횟수
     
 	void  Awake()
     {
         openTimeText.text = "개방 시간 : " + openTime + " ~ " + closeTime;
-        endPoint.SetActive(false);
+        openTimer.size = 1 - (openTime / closeTime);  // 시간 비율로 사이즈 잡아줌.
+
         Init();
     }
 	
@@ -39,8 +47,10 @@ public class Timer : MonoBehaviour {
     // 시간을 카운트 한다.
     void CountTime()
     {
-        currentTime += Time.deltaTime;
+        currentTime += Time.deltaTime;                           // 현재 시간 저장
         currentTimeText.text = "현재 시간 : " + currentTime;
+
+        currentTimer.value = currentTime / closeTime;            // 0~1의 값을 갖게 나눠줌
 
         // 열리는 시간 ~ 닫히는 시간 사이에 있으면 종료점 활성화
         if (openTime <= currentTime && currentTime <= closeTime)
@@ -50,12 +60,36 @@ public class Timer : MonoBehaviour {
         else
         {
             endPoint.SetActive(false);
+
+        }
+
+        // 시간이 초과되면 타임사이클 1회 증가시키고 리셋
+        if (currentTimer.value >= 1)
+        {
+            ++timeCycle;
+            timeCycleText.text = "초과된 횟수 : " + timeCycle;
+            ResetTime();
         }
     }
 
+    // 현재 시간 초기화
+    void ResetTime()
+    {
+        currentTime = 0;
+        currentTimeText.text = "현재 시간 : 0";
+        currentTimer.value = 0;
+    }
+
+    // 완전 초기화 시킴
     void Init()
     {
         currentTime = 0;
         currentTimeText.text = "현재 시간 : 0";
+        currentTimer.value = 0;
+
+        timeCycle = 0;
+        timeCycleText.text = "초과된 횟수 : 0";
+
+        endPoint.SetActive(false);
     }
 }
