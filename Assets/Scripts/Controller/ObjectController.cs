21a0db04dfa7f491aa66f880718b69d3c2f56d5c
djzwns <hjjshg@game.hoseo.edu>
2016-04-03 15:55:00 +0900

    캔버스 이름 RotationCanvas -> StatCanvas 로 변경하고 힘 조절이 필요한 오브젝트에 PowerSlider 추가

--- Assets/Scripts/Controller/ObjectController.cs
+++ Assets/Scripts/Controller/ObjectController.cs
@@ -3,22 +3,28 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ObjectController : MonoBehaviour {
-    bool isDrop;
-    bool clickedObject;
-    bool dropPossible;
+    bool isDrop;                // 내려놨는지 아닌지 상태를 나타냄.
+    bool clickedObject;         // 눌려있는지 아닌지, 게이지 바를 띄워준다.
+    bool dropPossible;          // 내려두려는 위치에 내려둘수있는지 체크
 
-    GameObject clickObj;
+    // GetComponents 사용시 0은 회전, 1은 파워 조절 -----------------------------
+    // ex) GetComponentsInChildren<Slider>()[POWER].value 
+    // 파워인덱스는 1을 뜻하며 동일 컴퍼넌트에서 두번째 컴퍼넌트를 사용한다는 의미
+    const int ROTATION = 0;
+    const int POWER = 1;
+    // --------------------------------------------------------------------------
+    
 
-    public float rot = 0.5f;
+    float value = 0.5f;         // 슬라이더의 값이 0~1인데 이것을 -0.5~0.5 사이에서 쓰기위해 뺴주는 값
     public GameObject userObject;
-    public Canvas rotationCanvas;
+    public Canvas statCanvas;   // 회전, 파워 관리하는 캔버스
 
     void Awake()
     {
         //rotationCanvas = gameObject.GetComponentInChildren<Canvas>();
         dropPossible = true;
         clickedObject = false;
-        rotationCanvas.enabled = false;
+        statCanvas.enabled = false;
     }
     
     // 업뎃
@@ -29,7 +35,6 @@ public class ObjectController : MonoBehaviour {
 
         if (Input.GetMouseButtonUp(0) && (gameObject.name.Contains(GetClickedObject().name)))
         {
-            clickObj = GetClickedObject();
             if (!clickedObject)
                 ObjectClickOn();
             else
@@ -73,16 +78,22 @@ public class ObjectController : MonoBehaviour {
     // 오브젝트 회전
     void RotationObject()
     {
-        float sliderVal = (rotationCanvas.GetComponentInChildren<Slider>().value - rot) / rot;
+        float sliderVal = (statCanvas.GetComponentsInChildren<Slider>()[ROTATION].value - value);
         // 슬라이더 값만큼 z축을 회전시킴 최대 90도
         userObject.transform.rotation = Quaternion.AngleAxis (sliderVal * 180f, new Vector3( 0, 0, 1f));
+    }
+
+    // 파워 값을 반환해준다. 범위 ( -0.5 ~ 0.5 )
+    public float GetPowerObject()
+    {
+        return statCanvas.GetComponentsInChildren<Slider>()[POWER].value - value;
     }
 
     // 오브젝트 클릭 on/ off 관련 --------------------------------------------
     void ObjectClickOn()
     {
         clickedObject = true;
-        rotationCanvas.enabled = true;
+        statCanvas.enabled = true;
     }
 
     //void ObjectClick(GameObject obj)
@@ -94,7 +105,7 @@ public class ObjectController : MonoBehaviour {
     void ObjectClickOff()
     {
         clickedObject = false;
-        rotationCanvas.enabled = false;
+        statCanvas.enabled = false;
     }
     // -----------------------------------------------------------------------
 
