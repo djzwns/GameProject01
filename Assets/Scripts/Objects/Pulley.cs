9dc7ed0531f3fe22d7bcab645f9f783674b8350b
djzwns <hjjshg@game.hoseo.edu>
2016-04-03 19:59:58 +0900

    물레방아 회전 방향 속도 조절 추가

--- Assets/Scripts/Objects/Pulley.cs
+++ Assets/Scripts/Objects/Pulley.cs
@@ -2,29 +2,40 @@
 using System.Collections;
 
 public class Pulley : MonoBehaviour {
-    public float speed = 1f;
+    float speed;                // 실제로 돌려주는 속도
+    public float power = 50f;    // 돌아가는 힘, 즉 회전 속도
+    
 
-	// Use this for initialization
-	void Awake ()
+    // Use this for initialization
+    void Awake()
     {
         // 유저 오브젝트면 콜라이더 끄기, 다른 오브젝트에 겹치게 할 수 있다.
         if (gameObject.tag != "Preview")
-            gameObject.GetComponent<Collider2D>().enabled = false;
+        {
+            gameObject.GetComponent<Collider2D>().enabled = true;
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
 
+        }
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        speed = gameObject.GetComponentInParent<ObjectController>().GetPowerObject() * power;
         // 계속 돌아가게 만듬
-        if (gameObject.GetComponentInParent<ObjectController>().IsDrop())
+        //if (gameObject.GetComponentInParent<ObjectController>().IsDrop() && speed != 0)
+        //{
+        //}
+        if (gameObject.tag != "Preview")
         {
             RotPulley();
         }
-	}
+    }
 
     // 물레방아? 도르래? 아무튼 회전시킨다.
     void RotPulley()
     {
+        // Vector3.forward : z축으로 회전 시킴
         transform.Rotate(Vector3.forward * Time.deltaTime * speed);
     }
 }
