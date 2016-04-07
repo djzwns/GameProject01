12f9a12a1819a280a66f707bc843e737f2910003
djzwns <hjjshg@game.hoseo.edu>
2016-04-08 00:28:02 +0900

    추 구현. 아직 미완성 수준

---
+++ Assets/Scripts/Objects/Pendulum.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pendulum : MonoBehaviour {
+    Rigidbody2D rigid2d;
+    ObjectController controller;
+
+    public float leftRange;
+    public float rightRange;
+    public float velocityThreshold;
+
+	// Use this for initialization
+	void Awake () {
+        rigid2d = GetComponent<Rigidbody2D>();
+        rigid2d.angularVelocity = velocityThreshold;
+        controller = gameObject.GetComponent<ObjectController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.IsDrop())
+        {
+            //MeltPosRot();
+            Push();
+        }
+        if (controller.IsClicked())
+        {
+            //FrozenPosRot();
+        }
+
+    }
+
+    //// 포지션, 회전 막음
+    //void FrozenPosRot()
+    //{
+    //    rigid2d.constraints = RigidbodyConstraints2D.FreezeAll;
+    //}
+
+    //// 포지션, 회전 풀어줌
+    //void MeltPosRot()
+    //{
+    //    rigid2d.constraints = ~RigidbodyConstraints2D.FreezeAll;
+    //}
+
+    // 추를 밀어주는 함수..
+    public void Push()
+    {
+        //Debug.Log(body2d.angularVelocity);
+        // 오른쪽으로 밀어줌.
+        if (transform.rotation.z > 0
+            && transform.rotation.z < rightRange
+            && (rigid2d.angularVelocity > 0)
+            && rigid2d.angularVelocity < velocityThreshold)
+        {
+            rigid2d.angularVelocity = velocityThreshold;
+        }
+        // 왼쪽으로 밀어줌
+        else if (transform.rotation.z < 0
+            && transform.rotation.z > leftRange
+            && (rigid2d.angularVelocity < 0)
+            && rigid2d.angularVelocity > velocityThreshold * -1)
+        {
+            rigid2d.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
