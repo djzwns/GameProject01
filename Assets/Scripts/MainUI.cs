c89c2810dd0139517f0a7758b69e40c9dcacab7d
djzwns <hjjshg@game.hoseo.edu>
2016-04-09 23:01:55 +0900

    씬 추가 및 이름 변경 main 씬 추가 stage 씬 -> game 씬으로 변경 메인화면에 쓸 텍스쳐 추가. 실행 종료 버튼 추가

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartClick()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    public void ExitClick()
+    {
+        Application.Quit();
+    }
+}
