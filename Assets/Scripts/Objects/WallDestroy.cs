2424e54220f854841132b4a6dab3d698608d4aeb
djzwns <hjjshg@game.hoseo.edu>
2016-04-11 18:53:29 +0900

    음.. 수정중이었으나,, 일단 보류 Destroy 사용중..

--- Assets/Scripts/Objects/WallDestroy.cs
+++ Assets/Scripts/Objects/WallDestroy.cs
@@ -2,23 +2,50 @@
 using System.Collections;
 
 public class WallDestroy : MonoBehaviour {
-
     public float destroyTime = 0.2f;
     //public float respawnTime = 3f;
-    float timer = 0f;
-    
+    //float timer = 0f;
+    //float respawnTimer = 0f;
+
+    //public GameObject objSelf;
+    //bool isActive = true;
+
+    void Update()
+    {
+        //if (!isActive)
+        //{
+        //    respawnTimer += Time.deltaTime;
+        //    if (respawnTimer > respawnTime)
+        //    {
+        //        gameObject.SetActive(true);
+        //        isActive = true;
+        //    }
+        //}
+        //if (isCrash)
+        //{
+        //    timer += Time.deltaTime;
+        //    if (timer > destroyTime)
+        //    {
+        //        gameObject.SetActive(false);
+        //        timer = 0f;
+        //        isActive = false;
+        //    }
+        //}
+    }
+
 
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Ball")
         {
-            timer += Time.deltaTime;
-            if (timer > destroyTime) 
-            {
-                gameObject.SetActive(false);
-                timer = 0f;
-            }
-            //Destroy(gameObject, destroyTime);
+            //timer += Time.deltaTime;
+            //if (timer > destroyTime)
+            //{
+            //    gameObject.SetActive(false);
+            //    timer = 0f;
+            //    isActive = false;
+            //}
+            Destroy(gameObject, destroyTime);
         }
     }
 }
