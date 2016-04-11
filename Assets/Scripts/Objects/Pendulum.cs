ebd42f3e4041d6b793bd11ef80805b1dcc5e7643
djzwns <hjjshg@game.hoseo.edu>
2016-04-11 13:12:42 +0900

    유저가 쓰는 추에 문제가 있어서 수정헀음

--- Assets/Scripts/Objects/Pendulum.cs
+++ Assets/Scripts/Objects/Pendulum.cs
@@ -7,61 +7,74 @@ public class Pendulum : MonoBehaviour {
 
     public float leftRange;
     public float rightRange;
-    public float velocityThreshold;
+    public float power;
+
+    public GameObject fixedWall;
 
 	// Use this for initialization
 	void Awake () {
         rigid2d = GetComponent<Rigidbody2D>();
-        rigid2d.angularVelocity = velocityThreshold;
+        rigid2d.angularVelocity = power;
         controller = gameObject.GetComponent<ObjectController>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (controller.IsDrop())
+        if (gameObject.tag != "UserObject")
         {
-            //MeltPosRot();
             Push();
         }
-        if (controller.IsClicked())
+        if (!controller.IsDrop())
         {
-            //FrozenPosRot();
+            fixedWall.transform.position = transform.position;
+            // 내려놓기 전까지 움직임 없게 포지션, 회전 고정시킴
+            if (rigid2d.constraints != RigidbodyConstraints2D.FreezeAll)
+                FrozenPosRot();
         }
-
+        else
+        {
+            Push();
+            // 내려뒀으니깐 회전, 포지션 풀어줌
+            if (rigid2d.constraints == RigidbodyConstraints2D.FreezeAll)
+                MeltPosRot();
+        }
     }
 
-    //// 포지션, 회전 막음
-    //void FrozenPosRot()
-    //{
-    //    rigid2d.constraints = RigidbodyConstraints2D.FreezeAll;
-    //}
+    // 포지션, 회전 막음
+    void FrozenPosRot()
+    {
+        rigid2d.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
 
-    //// 포지션, 회전 풀어줌
-    //void MeltPosRot()
-    //{
-    //    rigid2d.constraints = ~RigidbodyConstraints2D.FreezeAll;
-    //}
+    // 포지션, 회전 풀어줌
+    void MeltPosRot()
+    {
+        rigid2d.constraints = ~RigidbodyConstraints2D.FreezeAll;
+    }
 
     // 추를 밀어주는 함수..
     public void Push()
     {
+        if (gameObject.tag == "UserObject")
+            power = (gameObject.GetComponentInParent<ObjectController>().GetPowerObject()) * 100f;
+
         //Debug.Log(body2d.angularVelocity);
         // 오른쪽으로 밀어줌.
         if (transform.rotation.z > 0
             && transform.rotation.z < rightRange
             && (rigid2d.angularVelocity > 0)
-            && rigid2d.angularVelocity < velocityThreshold)
+            && rigid2d.angularVelocity < power)
         {
-            rigid2d.angularVelocity = velocityThreshold;
+            rigid2d.angularVelocity = power;
         }
         // 왼쪽으로 밀어줌
         else if (transform.rotation.z < 0
             && transform.rotation.z > leftRange
             && (rigid2d.angularVelocity < 0)
-            && rigid2d.angularVelocity > velocityThreshold * -1)
+            && rigid2d.angularVelocity > power * -1)
         {
-            rigid2d.angularVelocity = velocityThreshold * -1;
+            rigid2d.angularVelocity = power * -1;
         }
     }
 }
