10c45975bd9836c1ba54a067d3ab574cefb56e4d
djzwns <hjjshg@game.hoseo.edu>
2016-04-11 12:25:14 +0900

    스테이지용 유저용 도르래 수정 및 사이즈 조정 스크립트 : 회전 속도 조절

--- Assets/Scripts/Objects/Pulley.cs
+++ Assets/Scripts/Objects/Pulley.cs
@@ -3,30 +3,18 @@ using System.Collections;
 
 public class Pulley : MonoBehaviour {
     float speed;                // 실제로 돌려주는 속도
-    public float power = 50f;    // 돌아가는 힘, 즉 회전 속도
+    public float power = 100f;    // 돌아가는 힘, 즉 회전 속도
     
-
-    // Use this for initialization
-    void Awake()
-    {
-        // 유저 오브젝트면 콜라이더 끄기, 다른 오브젝트에 겹치게 할 수 있다.
-        if (gameObject.tag != "Preview")
-        {
-            gameObject.GetComponent<Collider2D>().enabled = true;
-            gameObject.GetComponent<Collider2D>().isTrigger = true;
-
-        }
-    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        speed = (gameObject.GetComponentInParent<ObjectController>().GetPowerObject()-0.5f) * power;
-        // 계속 돌아가게 만듬
-        //if (gameObject.GetComponentInParent<ObjectController>().IsDrop() && speed != 0)
-        //{
-        //}
-            RotPulley();
+        if (gameObject.tag == "UserObject")
+            speed = (gameObject.GetComponentInParent<ObjectController>().GetPowerObject()-0.5f) * power;
+        else
+            speed = power;
+
+        RotPulley();
     }
 
     // 물레방아? 도르래? 아무튼 회전시킨다.
