bffb20214ef7806210c6810151e9b4c1d486c72f
djzwns <hjjshg@game.hoseo.edu>
2016-04-13 17:38:14 +0900

    버섯 점프애니메이션 추가

--- Assets/Scripts/Objects/Jump.cs
+++ Assets/Scripts/Objects/Jump.cs
@@ -5,10 +5,12 @@ public class Jump : MonoBehaviour
 {
     float power = 1f;
     public float _power = 10f;
+    private Animator anim;
 
     void Awake()
     {
         //gameObject.GetComponent<EdgeCollider2D>().sharedMaterial.bounciness = power;
+        anim = this.GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -26,13 +28,22 @@ public class Jump : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D obj)
     {
-        if (gameObject.tag == "UserOpject") power = (gameObject.GetComponentInParent<ObjectController>().GetPowerObject()) * 10f;
+        if (gameObject.tag == "UserObject") power = (gameObject.GetComponentInParent<ObjectController>().GetPowerObject()) * 5f;
         else power = _power;
         if (obj.gameObject.tag == "Ball")
         {
             //gameObject.GetComponent<EdgeCollider2D>().sharedMaterial.bounciness = power;
             //Debug.Log(power);
+            anim.SetBool("isJumping", true);
             obj.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Ball")
+        {
+            anim.SetBool("isJumping", false);
         }
     }
 }
