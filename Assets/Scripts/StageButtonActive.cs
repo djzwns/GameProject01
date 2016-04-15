0ef55e1f5856bd16f8c3e4358a6bebea91cd7cbb
djzwns <hjjshg@game.hoseo.edu>
2016-04-15 21:21:31 +0900

    스테이지 선택창 이미지 변경 버튼에 스테이지 번호, 클리어 했는지 안했는지 표시하게 바꿈

--- Assets/Scripts/StageButtonActive.cs
+++ Assets/Scripts/StageButtonActive.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 public class StageButtonActive : MonoBehaviour {
     public StageManager stage;
     public Button[] button;
+    public Sprite[] sprite;
     int reachStage;
+    int MAXSTAGE;
+
+    const int CLEAR = 0;
+    const int NOTHING = 1;
+    const int CLOSE = 2;
 
 	// Use this for initialization
 	void Start () {
+        MAXSTAGE = stage.lastStage;
         ButtonInteractable();
 	}
 
@@ -16,9 +23,23 @@ public class StageButtonActive : MonoBehaviour {
     public void ButtonInteractable()
     {
         reachStage = stage.reachStage;
-        for (int i = 0; i < reachStage; ++i)
+        // clear noting close 이미지 변경해줌
+        for (int i = 0; i < MAXSTAGE; ++i)
         {
-            button[i].interactable = true;
+            //button[i].GetComponentsInChildren<Image>()[1].sprite = Resources.Load("Textures/StageSelect/" + (i+1) ) as Sprite;
+            // 클리어한 스테이지들 clear 이미지 사용
+            if (i < reachStage)
+            {
+                button[i].GetComponentsInChildren<Image>()[2].sprite = sprite[CLEAR];
+                button[i].interactable = true;
+            }
+
+            // 앞에 스테이지 못깨서 접속 못 하는 부분은 close
+            else
+                button[i].GetComponentsInChildren<Image>()[2].sprite = sprite[CLOSE];
         }
+        // 스테이지에 들어갈 수는 있지만 아직 클리어 하지 않은 부분은 빈 이미지 사용
+        if( MAXSTAGE > reachStage )
+            button[reachStage-1].GetComponentsInChildren<Image>()[2].sprite = sprite[NOTHING];
     }
 }
