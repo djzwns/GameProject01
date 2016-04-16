b5ba50b6bac7815ac39eb20bfd18405870998e53
djzwns <hjjshg@game.hoseo.edu>
2016-04-16 16:55:54 +0900

    Revert "뒤로가기할 때 숫자 이미지 초기화 되게 추가"
    This reverts commit 6cabb0624c0b12059a1d943e3754ff442a4cbad0.

--- Assets/Scripts/NumberPrint.cs
+++ Assets/Scripts/NumberPrint.cs
@@ -47,12 +47,4 @@ public class NumberPrint : MonoBehaviour {
         closeTime[0].sprite = number[timer.GetTime(2) / 10];
         closeTime[1].sprite = number[timer.GetTime(2) % 10];
     }
-
-    public void ResetTimePrint()
-    {
-        openTime[0].sprite = number[0]; // 10의자리
-        openTime[1].sprite = number[0]; // 1의 자리
-        closeTime[0].sprite = number[0];
-        closeTime[1].sprite = number[0];
-    }
 }
